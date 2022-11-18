c70933b0dc51c9871042447b865767c9fcf0bd81
taipt <phamtientai0311@gmail.com>
2022-11-18 09:10:21 +0700

    ...

--- src/layouts/DefaultLayout/Header/index.js
+++ src/layouts/DefaultLayout/Header/index.js
@@ -1,14 +1,31 @@
 import classNames from 'classnames/bind';
 import styles from './header.module.scss';
 
+import Dropdown from 'react-bootstrap/Dropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const cx = classNames.bind(styles);
 
 function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                <div className={cx('inner-page')}>Home</div>
-                <div className={cx('inner-name')}>Name</div>
+                <div className={cx('inner-page')}>Home {'>'} </div>
+
+                <div className={cx('inner-name')}>
+                    <div>Name</div>
+                    <div>
+                        <Dropdown style={{ fontSize: 24, color: 'white' }}>
+                            <Dropdown.Toggle variant="" id="dropdown-basic" />
+
+                            <Dropdown.Menu>
+                                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </div>
+                </div>
             </div>
         </header>
     );
--- src/layouts/DefaultLayout/Slidebar/index.js
+++ src/layouts/DefaultLayout/Slidebar/index.js
@@ -1,5 +1,29 @@
+import styles from './sideBar.module.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
 function Sidebar() {
-    return <h1>Sidebar</h1>;
+    return (
+        <aside className={cx('wrapper')}>
+            <div className={cx('logo')}>
+                <img
+                    src="https://vcdn1-kinhdoanh.vnecdn.net/2020/01/16/12-4472-1579159249-1579160129-3325-6795-1579160166.png?w=0&h=0&q=100&dpr=2&fit=crop&s=D8mMIyEsQ_DZ66aSj0yNjQ"
+                    alt=""
+                />
+                <div>Online Asset Management</div>
+            </div>
+
+            <div className={cx('content')}>
+                <div className={cx('home')}>Home</div>
+                <div className={cx('user')}>Manage User</div>
+                <div className={cx('asset')}>Manage Asset</div>
+                <div className={cx('assignment')}>Manage Assignment</div>
+                <div className={cx('request')}>Request For Returning</div>
+                <div className={cx('return')}>Return</div>
+            </div>
+        </aside>
+    );
 }
 
 export default Sidebar;
